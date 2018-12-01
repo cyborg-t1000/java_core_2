47fc6bb72a0d4d9925b2801edc904e1660344498
Maxim Nekrasov <kgtu@mail.ru>
2018-11-30 21:51:46 -0500

    Homework 3, task 2

--- src/main/java/lesson3/Main.java
+++ src/main/java/lesson3/Main.java
@@ -1,13 +1,15 @@
 package lesson3;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
 
-        // Прочитать файл (около 50 байт) в байтовый массив и вывести этот массив в консоль;
+        // 1. Прочитать файл (около 50 байт) в байтовый массив и вывести этот массив в консоль
+
         byte[] ba = new byte[50];
         try (FileInputStream in = new FileInputStream("files/test.txt")) {
             int x, i = 0;
@@ -18,6 +20,44 @@ public class Main {
             e.printStackTrace();
         }
         System.out.println(Arrays.toString(ba));
+
+        // 2. Последовательно сшить 5 файлов в один (файлы примерно 100 байт)
+
+        String fileOut = "files/out2.txt";
+        FileOutputStream fos = null;
+        ArrayList<InputStream> al = new ArrayList<>();
+        try {
+            al.add(new FileInputStream("files/test1.txt"));
+            al.add(new FileInputStream("files/test2.txt"));
+            al.add(new FileInputStream("files/test3.txt"));
+            al.add(new FileInputStream("files/test4.txt"));
+            al.add(new FileInputStream("files/test5.txt"));
+            fos = new FileOutputStream(fileOut);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        SequenceInputStream in = new SequenceInputStream(Collections.enumeration(al));
+        int x;
+        while (true) {
+            try {
+                if ((x = in.read()) == -1) break;
+                if (fos != null) {
+                    fos.write(x);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            in.close();
+            if (fos != null) {
+                fos.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
 
     }
 }
